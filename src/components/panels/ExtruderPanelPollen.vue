c26a19033a9887a86ea199ca55e1b310b557b0da
Philippe Durix <>
2022-08-18 17:16:10 +0200

    Minor change on extruders panel

--- src/components/panels/ExtruderPanelPollen.vue
+++ src/components/panels/ExtruderPanelPollen.vue
@@ -15,58 +15,63 @@
 		</v-card-title>
 
 		<v-card-text class="d-flex flex-column v-card__text--with-rows-highlighted">
-			<v-row class="flex-column row--highlighted">
-        <div class="flex-column">
-					<div class="center-label">XX tr/min</div>
-					<percentage-input></percentage-input>
-        </div>
-        <v-row class="px-0 py-0" dense>
-          <v-col cols="6">
-            <code-btn block color="primary" code="TODO" tile>
-              Extrude <v-icon class="mr-1">mdi-arrow-down-bold</v-icon>
-            </code-btn>
-          </v-col>
-          <v-col cols="6">
-            <code-btn block color="primary" code="TODO" tile>
-              Retract <v-icon class="mr-1">mdi-arrow-up-bold</v-icon>
-            </code-btn>
-          </v-col>
-        </v-row>
-        <v-row class="px-0 py-0" dense>
-          <v-col cols="3">
-            Mixer
-          </v-col>
-          <v-col cols="9">
-           <percentage-input></percentage-input>
-          </v-col>
-        </v-row>
-			</v-row>
+			<v-row class="row--highlighted">
+        <v-col cols="12 d-flex flex-column">
+          <div class="center-label">XX tr/min</div>
+          <percentage-input></percentage-input>
+        </v-col>
+      </v-row>
+      <v-row class="row--highlighted" dense>
+        <v-col cols="6">
+          <code-btn block color="primary" code="TODO" tile>
+            Extrude <v-icon class="mr-1">mdi-arrow-down-bold</v-icon>
+          </code-btn>
+        </v-col>
+        <v-col cols="6">
+          <code-btn block color="primary" code="TODO" tile>
+            Retract <v-icon class="mr-1">mdi-arrow-up-bold</v-icon>
+          </code-btn>
+        </v-col>
+      </v-row>
+      <v-row class="row--highlighted">
+        <v-col cols="3 d-flex align-center">
+          Mixer
+        </v-col>
+        <v-col cols="9 d-flex align-center">
+         <percentage-input></percentage-input>
+        </v-col>
+      </v-row>
 			<v-row>
-        <v-col cols="3">
+        <v-col cols="3 d-flex align-center">
           Feeder
         </v-col>
-        <v-col cols="9">
+        <v-col cols="9 d-flex align-center">
          XX / <tool-input style="display: inline-block;"  active></tool-input>
         </v-col>
 			</v-row>
 			<v-row class="row--highlighted justify-space-between align-center">
-        <v-col cols="3">
+        <v-col cols="3 d-flex align-center">
           Screw
         </v-col>
-        <v-col cols="9">
+        <v-col cols="9 d-flex align-center">
          XX / <tool-input style="display: inline-block;"  active></tool-input>
         </v-col>
 			</v-row>
 			<v-row class="justify-space-between align-center">
-        <v-col cols="3">
+        <v-col cols="3 d-flex align-center">
           Nozzle
         </v-col>
-        <v-col cols="9">
+        <v-col cols="9 d-flex align-center">
          XX / <tool-input style="display: inline-block;"  active></tool-input>
         </v-col>
 			</v-row>
-			<v-row class="row--highlighted align-center">
-				Extrusion Factor <percentage-input></percentage-input>
+			<v-row class="row--highlighted">
+        <v-col cols="3 d-flex align-center">
+          Extrusion Factor
+        </v-col>
+        <v-col cols="9 d-flex align-center">
+          <percentage-input></percentage-input>
+        </v-col>
 			</v-row>
 		</v-card-text>
 	</v-card>
