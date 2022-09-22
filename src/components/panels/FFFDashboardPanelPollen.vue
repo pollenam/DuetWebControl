fdf8660730d7094da1699c36286d89c4d6b30159
Philippe Durix <>
2022-09-22 09:47:20 +0200

    Minor change

--- src/components/panels/FFFDashboardPanelPollen.vue
+++ src/components/panels/FFFDashboardPanelPollen.vue
@@ -3,18 +3,18 @@
 		<v-col cols="12">
 			<v-row align="stretch">
 				<v-col cols="12" lg="6" class="d-flex">
-					<job-control-panel-pollen  class="w-100 mb-2"></job-control-panel-pollen>
+					<job-control-panel-pollen  class="w-100"></job-control-panel-pollen>
 				</v-col>
 				<v-col cols="12" md="6" lg="4" class="d-flex">
-					<high-temperature-panel-pollen  class="w-100 mb-2"></high-temperature-panel-pollen>
+					<high-temperature-panel-pollen  class="w-100"></high-temperature-panel-pollen>
 				</v-col>
 				<v-col cols="12" md="6" lg="2" class="d-flex">
-					<light-factor-pollen-panel  class="w-100 mb-2"></light-factor-pollen-panel>
+					<light-factor-pollen-panel  class="w-100"></light-factor-pollen-panel>
 				</v-col>
 			</v-row>
 			<v-row>
 				<v-col cols="12" md="9">
-					<build-surface-pollen-panel  class="mb-2"></build-surface-pollen-panel>
+					<build-surface-pollen-panel></build-surface-pollen-panel>
 				</v-col>
 				<v-col class="hidden-xs-only hidden-sm-only" md="3">
 					<macro-list-pollen></macro-list-pollen>
