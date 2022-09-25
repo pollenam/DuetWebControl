712da19320d428a480bed828fe320bdaa7246502
Philippe Durix <>
2022-09-25 18:51:07 +0200

    Working on #22: adds animation to console drawer

--- src/components/dialogs/ConsoleDrawerPollenDialog.vue
+++ src/components/dialogs/ConsoleDrawerPollenDialog.vue
@@ -4,6 +4,7 @@
   right: 0px;
   bottom: 0px;
   width: 300px;
+  height: 48px;
   z-index: 999;
 
   background: no-repeat center center fixed !important;
@@ -11,8 +12,27 @@
                     url('../../assets/honeyprint-sx2-background.jpg') !important;
   background-size: cover !important;
 
+  -webkit-transition: width 150ms;
+          transition: width 150ms;
+
+  .v-card__text {
+    height: 0px;
+    padding-top: 0px;
+    padding-bottom: 0px;
+
+    -webkit-transition: height 150ms;
+            transition: height 150ms;
+  }
+
   &.v-card--console-drawer-pollen--expanded {
     width: 60%;
+    height: auto;
+
+    .v-card__text {
+      height: calc(80vh - 48px);
+      padding-top: 16px;
+      padding-bottom: 16px;
+    }
   }
 
   .v-data-table__wrapper {
@@ -22,20 +42,20 @@
 }
 </style>
 <template>
-	<v-card elevation="0" class="v-card--console-drawer-pollen" :class="{ 'v-card--console-drawer-pollen--expanded': expanded }" v-if="isEnabled">
+	<v-card elevation="0" class="v-card--console-drawer-pollen hidden-md-and-down" :class="{ 'v-card--console-drawer-pollen--expanded': expanded }" v-if="isEnabled">
 		<v-card-title class="v-card__title--dense" @click="toggleExpanded">
 			<v-icon small class="mr-2">mdi-code-tags</v-icon> {{ $t('menu.control.console') }}
 		</v-card-title>
 
-		<v-card-text v-if="expanded">
-	<v-row :dense="$vuetify.breakpoint.mobile">
-		<v-col cols="12" class="pt-1">
-			<code-input solo></code-input>
-		</v-col>
-		<v-col cols="12">
-			<event-list></event-list>
-		</v-col>
-	</v-row>
+		<v-card-text>
+      <v-row :dense="$vuetify.breakpoint.mobile">
+        <v-col cols="12" class="pt-1">
+          <code-input solo></code-input>
+        </v-col>
+        <v-col cols="12">
+          <event-list></event-list>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
