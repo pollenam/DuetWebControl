3db68a6fd778b3d27fba54b4d6310c89f25decae
Unipto <max.carre6@hotmail.com>
2024-06-13 15:15:24 +0200

    R1.1.6 - Changing LED power using macro Set_Lights

--- src/components/panels/LightFactorPanelPollen.vue
+++ src/components/panels/LightFactorPanelPollen.vue
@@ -6,7 +6,7 @@
 		</v-card-title>
 
 		<v-card-text>
-			<percentage-input-pollen v-model="speedFactor" :min="0" :max="100" :step="1" :disabled="uiFrozen || fans[4] === null"></percentage-input-pollen>
+			<percentage-input-pollen :value="getLightPower()" @input="setLightPower($event)" :min="0" :max="100" :step="1" :disabled="uiFrozen || fans[4] === null"></percentage-input-pollen>
 		</v-card-text>
 	</v-card>
 </template>
@@ -20,18 +20,21 @@ export default {
 	computed: {
 		...mapGetters(['uiFrozen']),
 		...mapState('machine/model', ['fans']),
-		speedFactor: {
-			get() {
-				if(this.fans[4] != null)
-					return this.fans[4].requestedValue * 100;
-				return 0;
-			},
-			set(value) {
-				value = Math.min(100, Math.max(0, value)) / 100;
-				this.sendCode(`M106 P4 S${value.toFixed(2)}`);
-			}
-		}
 	},
-	methods: mapActions('machine', ['sendCode'])
+	methods: {
+		...mapActions('machine', ['sendCode']),
+		setLightPower(value) {
+			value = Math.min(100, Math.max(0, value)) / 100;
+			if(this.fans[4] != null){
+				this.sendCode(`M98 P"/macros/HONEYPRINT/Set_Lights" S${value.toFixed(2)}`);
+			}
+		},
+		getLightPower() {
+			if(this.fans[4] != null){
+				return this.fans[4].requestedValue * 100;
+			}
+			return 0;
+		},
+	}
 }
 </script>
